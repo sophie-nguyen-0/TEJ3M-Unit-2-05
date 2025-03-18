bf0b846b2ff9c2540b227ad51ca5305e66e6ecfb
Sophie Nguyen <113055711+sophie-nguyen-0@users.noreply.github.com>
2025-03-18 09:51:25 -0400

    changed pin to GP 19

--- code.py
+++ code.py
@@ -11,7 +11,7 @@ import pwmio
 from adafruit_motor import servo
 
 # create a PWMOut object on Pin A2.
-pwm = pwmio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
+pwm = pwmio.PWMOut(board.GP19, duty_cycle=2 ** 15, frequency=50)
 
 # Create a servo object, my_servo.
 my_servo = servo.Servo(pwm)
